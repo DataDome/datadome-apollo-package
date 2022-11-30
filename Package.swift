72d1154ed0cc89ee6e37d0f699ea39d0f24f8015
Kireyin <kireyin@icloud.com>
2022-11-30 16:04:13 +0100

    release 3.0.1 (#4)

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(name: "Apollo", url: "https://github.com/apollographql/apollo-ios", from: "0.19.1"),
-        .package(name: "DataDomeSDK", url: "https://github.com/DataDome/datadome-ios-package", from: "3.0.0")
+        .package(name: "DataDomeSDK", url: "https://github.com/DataDome/datadome-ios-package", from: "3.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
