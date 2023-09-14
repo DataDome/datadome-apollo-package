e22b88dfe21982f19b6f152d10aa1ce24e77b1b5
Alexandre Brispot <alexandre.brispot@datadome.co>
2023-09-14 10:46:13 +0200

    3.2.3

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apollographql/apollo-ios", exact: "1.1.3"),
-        .package(url: "https://github.com/DataDome/datadome-ios-package", from: "3.2.2")
+        .package(url: "https://github.com/DataDome/datadome-ios-package", from: "3.2.3")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
