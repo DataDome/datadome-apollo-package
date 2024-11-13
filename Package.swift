8191f4cdc157bd4f3116fcfdfceaa691eb5b175a
Kireyin <alexandre.brispot@datadome.co>
2024-11-13 16:06:11 +0100

    Update dependency to DD_SDK to 3.6.2 (#20)

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version:5.7
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -9,20 +8,16 @@ let package = Package(
         .iOS(.v12),
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "DataDomeApollo",
             targets: ["DataDomeApollo"]
         ),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apollographql/apollo-ios", from: Version(1, 0, 0)),
-        .package(url: "https://github.com/DataDome/datadome-ios-package", from: Version(3, 6, 1))
+        .package(url: "https://github.com/DataDome/datadome-ios-package", from: Version(3, 6, 2))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DataDomeApollo",
             dependencies: [
