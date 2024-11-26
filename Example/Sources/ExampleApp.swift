1cb8b688679e406b57f922788016ba054270c7c5
Alexandre Brispot <alexandre.brispot@datadome.co>
2024-11-26 17:30:19 +0100

    Add Example app

---
+++ Example/Sources/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Alexandre Brispot on 13/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Example/Sources/NetworkManager.swift
@@ -0,0 +1,65 @@
+//
+//  NetworkManager.swift
+//  Example
+//
+//  Created by Alexandre Brispot on 21/11/2024.
+//
+
+import Foundation
+
+import Apollo
+import DataDomeApollo
+
+final class NetworkManager {
+    enum Error: Swift.Error {
+        case unknowned
+    }
+    
+    static var shared: NetworkManager = NetworkManager()
+    
+    private let headers = [
+        "Accept": "application/json",
+        "User-Agent": "BLOCKUA", // For testing purpose only - This will force a Captcha challenge if no DataDome cookie is present
+        "Cache-Control": "max-age=0, no-cache, must-revalidate, proxy-revalidate" // For testing purpose only - This will bypass all cache
+    ]
+    
+    private(set) lazy var apollo: ApolloClient = {
+        // Create your own store needed to init the DataDomeInterceptor provider
+        let cache = InMemoryNormalizedCache()
+        let store = ApolloStore(cache: cache)
+        
+        // Configure your session client
+        let client = DataDomeURLSessionClient()
+        
+        // Create the DataDome Interceptor Provider
+        let provider = DataDomeInterceptorProvider(store: store, client: client)
+        
+        // Create your GraphQL URL
+        let wpJsonEndpoint = "https://datadome.co/wp-json"
+        
+        guard let url = URL(string: wpJsonEndpoint) else {
+            fatalError("Unable to create url https://datadome.co/wp-json")
+        }
+        
+        let requestChainTransport = RequestChainNetworkTransport(interceptorProvider: provider,
+                                                                 endpointURL: url,
+                                                                 additionalHeaders: headers,
+                                                                 useGETForQueries: true)
+        
+        // Create the client with the request chain transport
+        return ApolloClient(networkTransport: requestChainTransport,
+                            store: store)
+    }()
+        
+    private init() {
+        
+    }
+    
+    func protectedData(from url: URL, withId id: Int) async throws -> Data {
+        apollo.fetch(query: ApolloSchema.LaunchListQuery()) { result in
+            
+        }
+        
+        return "lksjdfg".data(using: .utf8)!
+    }
+}
